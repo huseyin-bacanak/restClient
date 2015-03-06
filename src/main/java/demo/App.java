9a9f905f3e21ed502c7fa917530ceedcc30be9d7
Hüseyin Bacanak <huseyin.bacanak@tpe.gov.tr>
2015-03-06 17:08:19 +0200

    initial

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,7 @@
+package demo;
+
+/**
+ * Created by huseyin.bacanak on 06.03.2015.
+ */
+public class App {
+}
---
+++ src/test/java/demo/RestTemplateTest.java
@@ -0,0 +1,66 @@
+package demo;
+
+
+import static junit.framework.TestCase.assertNotNull;
+
+import junit.framework.Assert;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClients;
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+public class RestTemplateTest {
+  @Test
+  public void getTest(){
+    String url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q={query}";
+    RestTemplate restTemplate= new RestTemplate();
+    restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+    String result = restTemplate.getForObject(url, String.class, "SpringSource");
+    assertNotNull(result);
+  }
+
+  @Test
+  public void getProcessInstances(){
+    String url = "http://localhost:9000/activiti/service/runtime/process-instances/";
+    CredentialsProvider credsProvider = new BasicCredentialsProvider();
+    credsProvider.setCredentials(
+        new AuthScope(null, -1),
+        new UsernamePasswordCredentials("kermit", "kermit"));
+    HttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+    RestTemplate restTemplate=new RestTemplate();
+    restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+
+    String result = restTemplate.getForObject(url, String.class);
+    Assert.assertNotNull(result);
+  }
+  @Test
+  public void postProcessInstance(){
+    String url = "http://localhost:9000/activiti/service/runtime/process-instances/";
+    CredentialsProvider credsProvider = new BasicCredentialsProvider();
+    credsProvider.setCredentials(
+        new AuthScope(null, -1),
+        new UsernamePasswordCredentials("kermit", "kermit"));
+    HttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+    RestTemplate restTemplate=new RestTemplate();
+    restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
+
+    MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+    map.add("username", "test");
+    map.add("password", "test123");
+    map.add("id", "1234");
+
+    String result = restTemplate.postForObject(url,map,  String.class);
+    Assert.assertNotNull(result);
+  }
+}
